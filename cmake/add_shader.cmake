09c3023b3ea9322c3b595289494ac8cc36690946
aDron1990 <adron1990@vk.com>
2025-02-12 21:18:44 +0300

    shaders dependensies

--- cmake/add_shader.cmake
+++ cmake/add_shader.cmake
@@ -3,7 +3,7 @@ function(add_shader shader_file)
     if (NOT GLSLC)
         message(FATAL_ERROR "glslc не найден. Убедитесь, что он установлен и доступен в PATH.")
     endif()
-    add_custom_command(
+    add_custom_command(PRE_BUILD
         COMMAND ${GLSLC} ${CMAKE_SOURCE_DIR}/${shader_file} -o ${CMAKE_SOURCE_DIR}/${shader_file}.spv
         DEPENDS ${shader_file}
         OUTPUT ${shader_file}.spv
