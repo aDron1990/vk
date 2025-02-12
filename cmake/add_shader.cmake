d30b363b61a77b3050fc84c4e58e743a0324281e
aDron1990 <adron1990@vk.com>
2025-02-12 15:18:11 +0300

    integrate shader compile to cmake

---
+++ cmake/add_shader.cmake
@@ -0,0 +1,13 @@
+function(add_shader shader_file)
+    find_program(GLSLC "glslc")
+    if (NOT GLSLC)
+        message(FATAL_ERROR "glslc не найден. Убедитесь, что он установлен и доступен в PATH.")
+    endif()
+    add_custom_command(
+        COMMAND ${GLSLC} ${CMAKE_SOURCE_DIR}/${shader_file} -o ${CMAKE_SOURCE_DIR}/${shader_file}.spv
+        DEPENDS ${shader_file}
+        OUTPUT ${shader_file}.spv
+        COMMENT "Copmiling shader: ${shader_file} -> ${shader_file}.spv"
+        VERBATIM
+    )
+endfunction()
--- resources/shaders/build.bat
+++
@@ -1,5 +0,0 @@
-glslc.exe shader.vert -o shader.vert.spv
-glslc.exe shader.frag -o shader.frag.spv
-glslc.exe post/shader.vert -o post/shader.vert.spv
-glslc.exe post/shader.frag -o post/shader.frag.spv
-pause
